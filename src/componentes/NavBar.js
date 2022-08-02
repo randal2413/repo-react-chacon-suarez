abb156130f5af35390b6b880d496ef4698a90953
Randal Chacon <randalchacon2413@gmail.com>
2022-08-02 20:17:58 -0300

    MENU ECOMMERCE

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+
+function NavBar() {
+    return (
+      <div className='navBar'>
+        <ul className='nav-menu'>
+         <h1>TIENDA DE ROPA</h1>
+          <li className="li">INICIO</li>
+          <li className="li">ROPA HOMBRE</li>
+          <li className="li">ROPA MUJER</li>
+          <li className="li">ROPA NIÑOS</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default NavBar
+  
